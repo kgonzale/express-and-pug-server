9e8cc5ca45102e524de6ec52bd7cc4463c81a4cf
Kevin Gonzalez <kgonzale@cbu.edu>
2019-01-20 17:42:52 -0600

    fixed time

--- server.js
+++ server.js
@@ -8,23 +8,36 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
 
 var ip = require("ip");
-var moment = require("moment");
+// var moment = require("moment");
 app.set("view engine", "pug");
 app.set("views", "./views");
 
 app.get("/", (req, res) => {
-  let time = moment().format("MMMM Do YYYY, h:mm:ss a");
+  // let time = moment().format("MMMM Do YYYY, h:mm:ss a");
   let uptime = require("os").uptime();
   let ipaddress = ip.address();
+
+  let sys = function getSysUptime() {
+    return (
+      parseFloat(
+        fs
+          .readFileSync("/proc/uptime", {
+            encoding: "utf8"
+          })
+          .split(" ")[0]
+      ) * 1000
+    );
+  };
 
   res.render("index", {
     title: "project 1",
     name: "Kevin Gonzalez",
-    currenttime: time,
     test: uptime,
-    address: ipaddress
+    address: ipaddress,
+    test: sys
   });
 });
 
---
+++ timer.js
@@ -0,0 +1,5 @@
+var moment = require("moment");
+
+setInterval(() => {
+  return moment().format("MMMM Do YYYY, h:mm:ss a");
+}, 1000);
